41a1e89b15d22c730eaa985e328f8c6960d3bb73
AndresBedon <45224284+AndresBedon@users.noreply.github.com>
2025-03-06 15:19:12 -0500

    galeria terminado

---
+++ src/app/common/slider-casa-forte-galeria/slider-casa-forte-galeria.component.ts
@@ -0,0 +1,80 @@
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule, NgClass, NgFor, NgIf, ViewportScroller } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { HomeService } from 'src/service/home/home.service';
+import { log } from 'console';
+
+@Component({
+    selector: 'app-slider-casa-forte-galeria',
+    imports: [RouterLink, NgClass, NgIf, NgFor, CommonModule, CarouselModule],
+    templateUrl: './slider-casa-forte-galeria.component.html',
+    styleUrls: ['./slider-casa-forte-galeria.component.scss']
+})
+export class SliderCasaForteGaleriaComponent implements OnInit, AfterViewInit  {
+  sliders: any = [];
+    constructor(
+        private viewportScroller: ViewportScroller,
+        private _firstComponentService: HomeService,
+        private cdr: ChangeDetectorRef
+    ) {}
+
+    public onClick(elementId: string): void { 
+        this.viewportScroller.scrollToAnchor(elementId);
+    }
+
+    homeSlides: OwlOptions = {
+            items: 1,
+            nav: false,
+            loop: true,
+            dots: true,
+            autoplay: false,
+            smartSpeed: 500,
+            autoHeight: true,
+            autoplayHoverPause: false,
+           /* navText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],*/
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+        }
+
+    ngOnInit(): void {
+      this.inicio();
+  }
+
+  inicio() {
+          this._firstComponentService.getComponentSlider()
+          .then((element) => {
+            //let response = element.data;
+            console.log('Elemento de inicio', element.data);
+            
+            this.sliders = element.data;
+        
+            // Detecta manualmente los cambios después de la actualización de los datos
+            this.cdr.detectChanges();
+        
+  
+          })
+          .catch((error) => {
+            console.error('Error al obtener el componente Inicio', error);
+          });
+      }
+
+  ngAfterViewInit() {
+       
+  }
+
+
+
+}
