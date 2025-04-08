96671bed13a0bf15ab52a3a5511a91bedbde9d9a
Jorge Carrillo <carrillojorgen12@gmail.com>
2025-04-08 02:30:39 -0500

    Update API URLs and assets to use ngrok for development
    Replaced hardcoded IP-based URLs with ngrok URLs across environments and assets for better flexibility during local development. This change affects API endpoints, images, and other configurations. Updated environment keys and removed unused Salesforce-related fields.

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,25 +2,13 @@ export const environment = {
   envkey: 'local',
   production: false,
   token:
-    '00ed836a8b7cb8638e2ad67bf858f18979786bbc501bfbd7bbf2dd2c83e37ef460ba046e9385eca86ae7124cd0c75b7803635ae49db0f033630da40695da9ba32a8ff963b85ac6523172c7c0ad2778b19797a1b748412f71b884a7b0dafc7c6660196896e6ed973ec768538bebca98928b810730533cef2be6c9dee31beb7d39',
+      'd630ba94b680938f37a551c8b0fc48dd0a7aad2da350efa5b38fef1118e00d46b90b73b7036c5aaef8cfc0a5f3afb504249fa8ceed2132862fc053d0b2e0ef3b978863edc9e774495fd2b4e547213747a914d469b6c9d5487d9bb0a20177c8191f2b5b6c44958f062cba684c82fc0a3844dc0766b857ff19caf0254ff8d04ca5',
   whatsapp: '',
-  api_url: 'https://prod-api-www.preupdv.cl/api',
-  api_img: 'https://prod-api-www.preupdv.cl',
-  api_salesforce_contacto:
-    'https://webto.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8&orgId=00D41000000c4lS',
-  api_salesforce_simulador:
-    'https://pdv--bdlacopia.sandbox.my.salesforce.com/services/data/v50.0/sobjects/Landing__c',
-  api_salesforce_simuladortoken:
-    'https://pdv.my.salesforce.com/services/data/v50.0/sobjects/Landing__c',
+  api_url: 'https://29d9f0f08711.ngrok.app/api',
+  api_img: 'https://29d9f0f08711.ngrok.app',
   grant_type: 'password',
-  client_id:
-    '3MVG9szVa2RxsqBZzaAXT3v2EtrUlnD9vvr84NesQ_hRArR9lTaExYeZIHt_H93VpIqLZkMyOAnU6xTKh6VBu',
-  client_secret:
-    '40672649A22592B70288933F285AE80EA895B2835F9877D7BE25696BC0921C7D',
-  username: 'crm@pedrodevaldivia.cl',
-  password: '5190zzzzmcjQgNxV6tMpYaHqH1D8Ziwq',
   title: 'Preuniversitario Pedro de Valdivia: Preparación para la PAES 2024',
-  GTM_ID: 'GTM-NJVPBHS',
-  url: 'https://localhost:4200',
-  sitemapUrl: 'https://localhost:4200/sitemap.xml',
+  url: 'https://29d9f0f08711.ngrok.app',
+  sitemapUrl: 'https://29d9f0f08711.ngrok.app/sitemap.xml',
+  apiGoogle: 'AIzaSyBXCDLujiJkJrac7EKVRq5yYM74dIvdvNA',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@ export const environment = {
   token:
     'd630ba94b680938f37a551c8b0fc48dd0a7aad2da350efa5b38fef1118e00d46b90b73b7036c5aaef8cfc0a5f3afb504249fa8ceed2132862fc053d0b2e0ef3b978863edc9e774495fd2b4e547213747a914d469b6c9d5487d9bb0a20177c8191f2b5b6c44958f062cba684c82fc0a3844dc0766b857ff19caf0254ff8d04ca5',
   whatsapp: '',
-  api_url: 'http://54.145.52.187:1338/api',
-  api_img: 'http://54.145.52.187:1338',
+  api_url: 'https://29d9f0f08711.ngrok.app/api',
+  api_img: 'https://29d9f0f08711.ngrok.app',
   grant_type: 'password',
   title: 'Preuniversitario Pedro de Valdivia: Preparación para la PAES 2024',
-  url: 'http://54.145.52.187:1338',
-  sitemapUrl: 'http://54.145.52.187:1338/sitemap.xml',
+  url: 'https://29d9f0f08711.ngrok.app',
+  sitemapUrl: 'https://29d9f0f08711.ngrok.app/sitemap.xml',
   apiGoogle: 'AIzaSyBXCDLujiJkJrac7EKVRq5yYM74dIvdvNA',
 };
